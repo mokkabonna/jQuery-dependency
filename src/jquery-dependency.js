f655e6aa6d2a00b29ec8acefe96735c89c7235ab
Martin Hansen <martin@martinhansen.no>
2014-01-14 15:51:29 +0100

    first tests added

--- src/jquery-dependency.js
+++ src/jquery-dependency.js
@@ -8,46 +8,56 @@
  */
 
 (function ($) {
-    $.fn.dependsOn = function (parent) {
-        if (parent === undefined) { console.log('Parent is required'); return; }
+	$.fn.dependsOn = function (parent) {
+		if (parent === undefined) {
+			throw 'Parent is required';
+		}
 
-        var opts = { parent: parent, value: null };
+		var opts = {
+			parent: parent,
+			value: null
+		};
 
-        if ($(opts.parent).attr('type') == 'radio') {
-            opts.origparent = $(opts.parent);
-            opts.parent = 'input[name=' + $(opts.parent).attr('name') + ']';
-        }
+		if ($(opts.parent).attr('type') === 'radio') {
+			opts.origparent = $(opts.parent);
+			opts.parent = 'input[name=' + $(opts.parent).attr('name') + ']';
+		}
 
-        return this.each(function () {
-            var caller = $(this);
-            $.data(this, 'dependsOnOptions', opts);
+		return this.each(function () {
+			var caller = $(this);
+			$.data(this, 'dependsOnOptions', opts);
 
-            caller.on('click iterate', function (event) {
-                var parent = (opts.origparent) ? opts.origparent : $(opts.parent);
-                parent.attr('checked', true).trigger('iterate', ['Iterate', 'Event']);
-                if (jQuery.ui) parent.button('refresh');
-            });
+			caller.on('click iterate', function () {
+				var parent = (opts.origparent) ? opts.origparent : $(opts.parent);
+				parent.attr('checked', true).trigger('iterate', ['Iterate', 'Event']);
+				if (jQuery.ui) {
+					parent.button('refresh');
+				}
+			});
 
-            $(opts.parent).each(function (i) {
-                var pp = $(this);
+			$(opts.parent).each(function () {
+				var pp = $(this);
 
-                var checked = pp.attr('checked');
-                if (checked) {
-                    $.fn.dependsOn.check(pp, caller, opts);
-                }
+				var checked = pp.attr('checked');
+				if (checked) {
+					$.fn.dependsOn.check(pp, caller, opts);
+				}
 
-                pp.on('change', function (event) {
-                    $.fn.dependsOn.check($(this), caller, opts);
-                });
-            });
+				pp.on('change', function () {
+					$.fn.dependsOn.check($(this), caller, opts);
+				});
+			});
 
-        });
-    };
+		});
+	};
 
-    $.fn.dependsOn.check = function (parent, child, opts) {
-        if (!parent.is(':checked') || (opts.origparent && !$(opts.origparent).is(':checked'))) {
-            child.attr('checked', false).trigger('change');
-            if (jQuery.ui) parent.button('refresh');
-        }
-    };
+	$.fn.dependsOn.check = function (parent, child, opts) {
+		if (!parent.is(':checked') || (opts.origparent && !$(opts.origparent).is(':checked'))) {
+			child.attr('checked', false).trigger('change');
+			if (jQuery.ui) {
+				parent.button('refresh');
+			}
+		}
+	};
+
 })(jQuery);
